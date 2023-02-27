c2f43b02120a9e73a7537959168628e6c32e4ffa
prathy08 <bashaboinaprathyusha08@gmail.com>
2023-02-27 11:04:53 +0530

    Add:wages

---
+++ empolyeewage/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace empolyeewage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EmployeeAttendence employeeAttendence = new EmployeeAttendence();
+            employeeAttendence.checkEmpolyeeAttendence();
+            employeeAttendence.calculate();
+                
+        }
+    }
+}
